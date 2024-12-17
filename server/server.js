51eb38222a410fb6cdf464ccd793e71a24d37e88
Srijana KC <srijanakhatri77@gmail.com>
2024-12-17 13:37:50 +0000

    add: updatedd

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { dbCon } from './utils/db.js';
-import routers from './routes/routes.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import { dbCon } from "./utils/db.js";
+import routers from "./routes/routes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -13,8 +13,8 @@ dbCon();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', routers);
+app.use("/api", routers);
 
-app.listen(process.env.PORT  || 4000,()=>{
-    console.log(`Server is running on port: ${process.env.PORT}`);
-})
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
+});
